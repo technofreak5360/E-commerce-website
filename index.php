e2f0a01e45f42f0009e49850da02be56fce80228
mmyadav <mintu08.yadav@gmail.com>
2019-09-18 03:04:21 +0530

    V1

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>E-commerece website</title>
+    <link rel="stylesheet" href="styles/reset.css" media="all">
+    <link rel="stylesheet" href="styles/style.css" media="all">
+  </head>
+  <body>
+<div class="main_wrapper">
+<div class="header_wraper">
+  <img id="logo" src="images/logo.gif" alt="logo">
+  <img  id="banner" src="images/ad_banner.gif" alt="banner">
+
+</div>
+<div class="menuebar">
+
+</div>
+<div class="content_wrapper">
+<div id="sidebar">
+
+</div>
+<div id="content_area">
+
+</div>
+
+</div>
+<div id="footer">
+
+</div>
+</div>
+  </body>
+</html>
