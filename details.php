a8c47a1853b74f7d4c1aa416911a24727b960e92
mmyadav <mintu08.yadav@gmail.com>
2019-09-25 01:45:14 +0530

    v3

--- details.php
+++ details.php
@@ -12,17 +12,17 @@ include("functions/functions.php");
   <body>
 <div class="main_wrapper">
 <div class="header_wraper">
-  <a href="index.php" > <img id="logo" src="images/logo.gif" alt="logo"> </a>
+  <img id="logo" src="images/logo.gif" alt="logo">
   <img  id="banner" src="images/ad_banner.gif" alt="banner">
 
 </div>
 <div class="menuebar">
 <ul id="menue">
-  <li><a href="index.php">Home</a></li>
-  <li> <a href="all_products.php">All products</a> </li>
-  <li> <a href="customer/my_account.php">My Account</a> </li>
+  <li><a href="#">Home</a></li>
+  <li> <a href="#">All products</a> </li>
+  <li> <a href="#">My Account</a> </li>
   <li> <a href="#">Sign up</a> </li>
-  <li> <a href="cart.php">Shopping Cart</a>  </li>
+  <li> <a href="#">Shopping Cart</a>  </li>
   <li><a href="#">contact us</a> </li>
 </ul>
 <div id="form">
@@ -53,11 +53,49 @@ include("functions/functions.php");
   </div>
   <div id="products_box">
 
-				<?php getPro(); ?>
-        	<?php getCatPro(); ?>
-          <?php getBrandPro(); ?>
+    <?php
+if(isset($_GET['pro_id'])){
 
-				</div>
+$product_id = $_GET['pro_id'];
+
+$get_pro = "select * from products where product_id='$product_id'";
+
+$run_pro = mysqli_query($con, $get_pro);
+
+while($row_pro=mysqli_fetch_array($run_pro)){
+
+  $pro_id = $row_pro['product_id'];
+  $pro_title = $row_pro['product_title'];
+  $pro_price = $row_pro['product_price'];
+  $pro_image = $row_pro['product_image'];
+  $pro_desc = $row_pro['product_desc'];
+
+  echo "
+      <div id='single_product'>
+
+        <h3>$pro_title</h3>
+
+        <img src='admin_area/product_images/$pro_image' width='400' height='300' />
+
+        <p><b> $ $pro_price </b></p>
+
+        <p>$pro_desc </p>
+
+        <a href='index.php' style='float:left;'>Go Back</a>
+
+        <a href='index.php?pro_id=$pro_id'><button style='float:right'>Add to Cart</button></a>
+
+      </div>
+
+
+  ";
+
+}
+}
+?>
+
+
+
 </div>
 
 </div>
--- functions/functions.php
+++ functions/functions.php
@@ -85,4 +85,109 @@ function getPro(){
 
 }
 
+function getCatPro(){
+
+	if(isset($_GET['cat'])){
+
+		$cat_id = $_GET['cat'];
+
+	global $con;
+
+	$get_cat_pro = "select * from products where product_cat='$cat_id'";
+
+	$run_cat_pro = mysqli_query($con, $get_cat_pro);
+
+	$count_cats = mysqli_num_rows($run_cat_pro);
+
+	if($count_cats==0){
+
+	echo "<h2 style='padding:20px;'>No products where found in this category!</h2>";
+
+	}
+
+	while($row_cat_pro=mysqli_fetch_array($run_cat_pro)){
+
+		$pro_id = $row_cat_pro['product_id'];
+		$pro_cat = $row_cat_pro['product_cat'];
+		$pro_brand = $row_cat_pro['product_brand'];
+		$pro_title = $row_cat_pro['product_title'];
+		$pro_price = $row_cat_pro['product_price'];
+		$pro_image = $row_cat_pro['product_image'];
+
+		echo "
+				<div id='single_product'>
+
+					<h3>$pro_title</h3>
+
+					<img src='admin_area/product_images/$pro_image' width='180' height='180' />
+
+					<p><b> $ $pro_price </b></p>
+
+					<a href='details.php?pro_id=$pro_id' style='float:left;'>Details</a>
+
+					<a href='index.php?pro_id=$pro_id'><button style='float:right'>Add to Cart</button></a>
+
+				</div>
+
+		";
+
+
+	}
+
+}
+
+}
+
+function getBrandPro(){
+
+	if(isset($_GET['brand'])){
+
+		$brand_id = $_GET['brand'];
+
+	global $con;
+
+	$get_brand_pro = "select * from products where product_brand='$brand_id'";
+
+	$run_brand_pro = mysqli_query($con, $get_brand_pro);
+
+	$count_brands = mysqli_num_rows($run_brand_pro);
+
+	if($count_brands==0){
+
+	echo "<h2 style='padding:20px;'>No products where found associated with this brand!!</h2>";
+
+	}
+
+	while($row_brand_pro=mysqli_fetch_array($run_brand_pro)){
+
+		$pro_id = $row_brand_pro['product_id'];
+		$pro_cat = $row_brand_pro['product_cat'];
+		$pro_brand = $row_brand_pro['product_brand'];
+		$pro_title = $row_brand_pro['product_title'];
+		$pro_price = $row_brand_pro['product_price'];
+		$pro_image = $row_brand_pro['product_image'];
+
+		echo "
+				<div id='single_product'>
+
+					<h3>$pro_title</h3>
+
+					<img src='admin_area/product_images/$pro_image' width='180' height='180' />
+
+					<p><b> $ $pro_price </b></p>
+
+					<a href='details.php?pro_id=$pro_id' style='float:left;'>Details</a>
+
+					<a href='index.php?pro_id=$pro_id'><button style='float:right'>Add to Cart</button></a>
+
+				</div>
+
+		";
+
+
+	}
+
+}
+}
+
  ?>
